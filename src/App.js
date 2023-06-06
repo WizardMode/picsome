2ba94dfcf5bbaf4cb0a28ebe51ad971b41c7fe54
WizardMode <humanspacebolt@live.com>
2023-06-06 16:58:48 -0700

    add components, pages, and utils

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
 function App() {
   return (
     <div>
-      
+      <Header />
+      <h1>Home Page</h1>
     </div>
   );
 }
