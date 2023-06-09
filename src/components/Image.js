9856182abcaedac0dbd7b454a04cc9ad84771b9c
WizardMode <humanspacebolt@live.com>
2023-06-08 23:03:38 -0700

    display icons on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,20 @@
+import {useState} from "react"
+
 function Image({className, img}) {
+    const [hovered, setHovered] = useState(false)
+
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
-        <div className={`${className} image-container`}>
+        <div 
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            className={`${className} image-container`}
+            >
             <img src={img.url} className="image-grid" />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
