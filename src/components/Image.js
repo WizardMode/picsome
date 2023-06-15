98bb72ce4191d7def7160ea547f4574379751797
WizardMode <humanspacebolt@live.com>
2023-06-14 19:02:32 -0700

    add to cart

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ import {Context} from "../Context"
 
 function Image({className, img}) {
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems} = useContext(Context)
 
     function heartIcon() {
         if(img.isFavorite) {
@@ -14,7 +14,14 @@ function Image({className, img}) {
         }
     }
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    function cartIcon() {
+        const alreadyInCart = cartItems.some(item => item.id === img.id)
+        if(alreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart"></i>
+        } else if(hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        }
+    }
 
     return (
         <div 
@@ -24,7 +31,7 @@ function Image({className, img}) {
             >
             <img src={img.url} className="image-grid" />
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
 }
