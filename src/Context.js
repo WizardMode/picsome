ae3dd7660313e9a91d83a55ba51cf4b3b8142a46
WizardMode <humanspacebolt@live.com>
2023-06-20 12:25:20 -0700

    Cart Page setup

--- src/Context.js
+++ src/Context.js
@@ -35,8 +35,19 @@ function ContextProvider({children}) {
         setCartItems(prevItems => prevItems.filter(item => item.id !== id))
     }
 
+    function emptyCart() {
+        setCartItems([])
+    }
+
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, removeFromCart, cartItems}}>
+        <Context.Provider value={{
+            allPhotos, 
+            toggleFavorite, 
+            addToCart, 
+            removeFromCart, 
+            cartItems, 
+            emptyCart
+        }}>
             {children}
         </Context.Provider>
     )
