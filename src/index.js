f060505b2f6fb35a119586903a573b285b036161
WizardMode <humanspacebolt@live.com>
2023-06-26 15:23:28 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router, HashRouter} from "react-router-dom"
 import {ContextProvider} from "./Context"
 import './index.css';
 import App from './App';
@@ -8,8 +8,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ContextProvider>
-    <Router>
+    {/*<Router>*/}
+    <HashRouter>
       <App />
-    </Router>
+     </HashRouter>
+    {/*</Router>*/}
   </ContextProvider>
 );
