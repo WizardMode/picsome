8d72cb059b8cd6e6e67df0f0a7e38681389a900f
WizardMode <humanspacebolt@live.com>
2023-06-28 21:19:53 -0700

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer>
             <a className="footer-link" href="https://leocreate.pythonanywhere.com/blog/">
-                <img src={icon} class="icon" />
+                <img src={icon} class="icon" alt="icon"/>
                 <p>Leonardo Ramirez 2023</p>
             </a>
         </footer>
